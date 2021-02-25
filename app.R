5bad6819c3ec03dabd67baa44a9f36db9dd7ece1
Okey <dynamicsoorya2691@gmail.com>
2021-02-25 17:29:36 -0500

    Made changed to map

--- app.R
+++ app.R
@@ -6,37 +6,21 @@ library(leaflet)
 library(leaflet.extras)
 library(dplyr)
 
-# increase max R-Shiny user-input file size from 5 to 30 MB
-options(shiny.maxRequestSize = 30 * 1024 ^ 2)
-
-# define fonts for plot
-f1 <- list(family = "Arial, sans-serif",
-           size = 24,
-           color = "black")
-
-f2 <- list(family = "Arial, sans-serif",
-           size = 20,
-           color = "black")
-
-f3 <- list(family = "Arial, sans-serif",
-           size = 16,
-           color = "black")
-
-# set plot margins
-m <- list(
-  l = 20,
-  r = 20,
-  b = 10,
-  t = 100,
-  pad = 4
-)
-
 # the ui object has all the information for the user-interface
 ui <-(
   navbarPage("Viral Space Time", id="main",
-             tabPanel("Map", leafletOutput("mymap", height=600), 
-                      absolutePanel(top = 160, left = 80,checkboxInput("heat", "Heatmap", FALSE)
-                      )),
+             tabPanel("Map", fluidPage(
+               fluidRow(
+                 column(2, 
+                        absolutePanel(top = 20, left = 30,checkboxInput("phy", "Load Phy", TRUE)),
+                        absolutePanel(top = 50, left = 30,checkboxInput("heat", "Load Temp Anomoly", FALSE))
+                        ),
+                 column(10,
+                        leafletOutput("mymap", height=700),
+                        absolutePanel(top = 20, left = 70,  actionButton("resetMap", "Reset")),
+                        )
+               )
+             )),
              tabPanel("Overview",includeMarkdown("readme.md")))
 )
 
@@ -44,32 +28,58 @@ ui <-(
 server <- function(input, output, session) {
   #import data
   data <- read.csv("datasets/curated/NOAAGlobalTemp/testdat.csv")
-  
+  phydata <- read.csv("outputs/quick_alr/latlon_north_america_phy.csv")
   #define the color pallate for the magnitidue of the earthquake
   pal <- colorNumeric(
     palette = c('blue', 'deep skyblue', 'cyan', 'orange red', 'red', 'dark red'),
     domain = data$mag)
   
+  
+  set_phy_color <- function(phyStr) {
+    def_color <- "blue"
+    if(phyStr == NULL) {
+      def_color <- "red"
+    }
+    return(def_color)
+  }
+  
   output$mymap <- renderLeaflet({
     leaflet(data) %>% 
-      setView(lng = -98.557, lat = 37.26 , zoom = 3) %>%
-      # Add two tiles
+      setView(lng = -99, lat = 32, zoom = 4)  %>%
       addProviderTiles("Esri.WorldImagery", group="Satellite Map") %>%
       addProviderTiles("CartoDB.DarkMatter", group="Dark Map") %>%
-      addTiles(options = providerTileOptions(noWrap = TRUE), group="Street Map") %>%
-      addCircles(data = data, lat = ~ LAT, lng = ~ LON, weight = 1, radius = 25000, popup = ~as.character(mag), label = ~as.character(paste0("Temp Anomoly: ", sep = " ", mag)), color = ~pal(mag), fillOpacity = 0.5) %>%
+      addTiles(options = providerTileOptions(noWrap = FALSE), group="Street Map") %>%
+      #addCircles(data = data, lat = ~ LAT, lng = ~ LON, weight = 1, radius = 3, popup = ~as.character(mag), label = ~as.character(paste0("Temp Anomoly: ", sep = " ", mag)), color = ~pal(mag), fillOpacity = 0.5) %>%
+      #addCircles(data = phydata, lat = ~ lat, lng = ~ lon, weight = 1, radius = 3, popup = "test", label = "Test", color = "blue", fillOpacity = 0.5) %>%
       addLayersControl(baseGroups = c("Dark Map","Satellite Map","Street Map"), options = layersControlOptions(collapsed = TRUE))
+      #fitBounds(260, -23, 90, 80) 
+  })
+  
+  observe({
+    proxy <- leafletProxy("mymap", data = phydata)
+    proxy %>% clearMarkers()
+    if (input$phy) {
+      proxy %>% addCircles(data = phydata, lat = ~ lat, lng = ~lon, weight = 1, radius = 3, popup = ~as.character(loc), label = ~as.character(paste0("Phy: ", sep = " ", loc)), color = ~set_phy_color(loc), fillOpacity = 0.5)
+      }
+    else {
+      proxy %>% clearShapes()
+      }
   })
   
   observe({
     proxy <- leafletProxy("mymap", data = data)
-    proxy %>% clearMarkers()
+    proxy %>% clearMarkers() 
     if (input$heat) {
-      proxy %>%  addHeatmap(lng=~LON, lat=~LAT, intensity = ~mag, blur =  3, max = 0.05, radius = 7) 
+      proxy %>%  addHeatmap(lng=~LON-270, lat=~LAT, intensity = ~mag, blur =  70, max = 10, radius = 40)
     }
     else{
       proxy %>% clearHeatmap()
     }
+  })
+  
+  observeEvent(input$resetMap, {
+    proxy <- leafletProxy("mymap", data = data)
+    proxy %>% setView(lng = -99, lat = 32, zoom = 4)
   })
 }
 
