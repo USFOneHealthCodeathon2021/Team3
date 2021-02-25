a9c494b7828947b378eaaf8e8801e8ccd92782b8
Ryan McMinds <r.mcminds@gmail.com>
2021-02-25 11:59:12 -0500

    Update quick_alr.r

--- scripts/quick_alr.r
+++ scripts/quick_alr.r
@@ -6,8 +6,20 @@ dat <- read.table('/Users/Ryan/codeathon2021/Team3/datasets/raw/NextStrain/nexts
 
 dat <- dat[dat$Location != '',]
 
+phy <- drop.tip(phy,phy$tip.label[!phy$tip.label %in% dat$Strain])
+
 library('tmaptools')
-#latlon <- geocode_OSM(dat$Location)
+#latlon <- geocode_OSM(unique(dat$Location))
 #write.table(latlon,file=file.path(getwd(),'datasets','curated','NextStrain','latlon_north_america.txt'),sep='\t',quote=FALSE,row.names=FALSE)
 read.table(file.path(getwd(),'datasets','curated','NextStrain','latlon_north_america.txt'),sep='\t',header=TRUE)
 
+library(geosphere)
+gcIntermediate(latlon[1,c(3,2)],latlon[2,c(3,2)],1)
+
+latlon_tips <- t(sapply(phy$tip.label,function(x) latlon[latlon$query==dat[dat$Strain==x,'Location'],c('lon','lat')]))
+
+                        library(phangorn)
+                        library(icosa)
+latlon_internal <- t(sapply(unique(phy$edge[,1]), function(x) surfacecentroid(matrix(as.numeric(latlon_tips[Descendants(phy,x,'tips')[[1]],]),ncol=2)))  )       
+                            
+                            
