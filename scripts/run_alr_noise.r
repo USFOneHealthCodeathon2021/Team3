d3b0bed77cf91e622d33f3cb717531e75f8c4ea2
Ryan McMinds <r.mcminds@gmail.com>
2021-02-27 22:36:34 -0500

    Update run_alr_noise.r

--- scripts/run_alr_noise.r
+++ scripts/run_alr_noise.r
@@ -5,7 +5,7 @@ library(rstan)
 wd <- getwd()
 output_prefix <- file.path(wd,'outputs/alr')
 model_dir <- file.path(wd, 'models')
-model_name <- 'alr_noise'
+model_name <- 'alr_noise_donut'
 
 sampling_command <- paste(paste0('./', model_name),
                 paste0('data file=', file.path(output_prefix, paste0(model_name, '_data.json'))),
@@ -35,7 +35,7 @@ latlon <- read.table(file.path(getwd(),'datasets','curated','NextStrain','latlon
 
 latlon$kappa <- apply(latlon[,c('lon_min','lon_max','lat_min','lat_max')], 1, function(x) {
   r <- geosphere:::distGeo(c(x[[1]],x[[3]]),c(x[[2]],x[[4]]), a=1) / 2
-  return(qnorm(0.975,0,1/sqrt(r)))
+  return(qnorm(0.99,0,1/sqrt(r)))
 })
 
 latlon_tips <- t(sapply(phy$tip.label, function(x) latlon[latlon$query==dat[dat$Strain==x,'location_combined'],c('lon','lat','kappa')]))                       
@@ -46,7 +46,7 @@ phy <- read.tree(file.path(output_prefix, 'nextstrain_ncov_north-america_timetre
 
 
 latlon_mat <- matrix(as.numeric(latlon_tips[phy$tip.label,]),ncol=3)
-latlonrad <- DescTools:::DegToRad(latlon_mat[,1:2]) 
+latlonrad <- latlon_mat[,1:2] / 180 * pi
 latlonrad[,2] <- (pi/2) - latlonrad[,2] ## geographic degrees go from -90 to +90 starting at equator, math starts at top and goes 0-pi
                         
 latlon_internal_guess <- t(sapply(unique(phy$edge[,1]), function(x) icosa:::surfacecentroid(matrix(as.numeric(latlon_tips[phangorn:::Descendants(phy,x,'tips')[[1]],1:2]),ncol=2))))   
@@ -54,7 +54,7 @@ latlon_internal_guess_rad <- DescTools:::DegToRad(latlon_internal_guess)
 latlon_internal_guess_rad[,2] <- (pi/2) - latlon_internal_guess_rad[,2]
                                   
 latlon_inits_rad <- rbind(latlonrad, latlon_internal_guess_rad)
-latlon_inits_cartesian <- t(apply(latlon_inits_rad, 1, function(x) c(sin(x[1])*cos(x[2]), sin(x[1])*sin(x[2]), cos(x[1])))) 
+latlon_inits_cartesian <- t(apply(latlon_inits_rad, 1, function(x) c(sin(x[2])*cos(x[1]), sin(x[2])*sin(x[1]), cos(x[2])))) 
 latlon_inits_cartesian_noise <- latlon_inits_cartesian + matrix(rnorm(nrow(latlon_inits_cartesian)*3,0,1e-4),ncol=3)
 latlon_inits_cartesian_noise <- t(apply(latlon_inits_cartesian_noise, 1, function(x) x / sqrt(sum(x^2))))                                   
       
@@ -69,7 +69,7 @@ phy$edge.length[phy$edge.length < time_min] <- time_min / 2
   
 ratevec <- as.vector((tipdist/cophenetic(phy))[lower.tri(tipdist)])
                              
-sigma_prior <- sqrt(mean(ratevec[ratevec > 0])) * 5
+sigma_prior <- sqrt(mean(ratevec[ratevec > 0])) 
 
                              
 data <- list(N = N,
@@ -101,7 +101,9 @@ check_hmc_diagnostics(stanfit)
                              
 summary(stanfit,pars='sigma_raw')
                              
-locations_cartesian <- apply(extract(stanfit,pars='loc')[[1]],c(2,3),mean)
+                             
+locations_cartesian_tensor <- extract(stanfit,pars='loc')[[1]]
+locations_cartesian <- t(apply(locations_cartesian_tensor, 2, function(x) Directional:::vmf.mle(x)$mu))                         
 locations_radians <- t(apply(locations_cartesian, 1, function(x) c(phi=atan2(x[2],x[1]),theta=atan2(sqrt(sum(x[1:2]^2)),x[3]))))
 locations_degrees <- locations_radians / pi * 180
 locations_degrees[,2] <- 90 - locations_degrees[,2]
@@ -109,7 +111,15 @@ colnames(locations_degrees) <- c('lon','lat')
 rownames(locations_degrees) <- c(rownames(latlon_tips), paste0('ancestor_',(N+1):(N+NI)))                             
 write.table(locations_degrees, file=file.path(output_prefix,'latlon_noise_tip_and_ancestors_north_america.txt'),sep='\t',quote=FALSE,row.names=FALSE)
 
-locdeg <- as.data.frame(locations_degrees)
+library(leaflet)
+
+#locdeg <- as.data.frame(locations_degrees)
+locdeg <- as.data.frame(locations_degrees[1:N,])
+#locdeg <- latlon_internal_guess
+#colnames(locdeg) <- c('lon','lat')
+#locdeg <- as.data.frame(latlon_mat)
+#colnames(locdeg) <- c('lon','lat','kappa')
 sp:::coordinates(locdeg) <- ~lon+lat
 leaflet(locdeg) %>% addMarkers() %>% addTiles()
+                             
 sigma <- mean(extract(stanfit,pars='sigma_raw')[[1]])
